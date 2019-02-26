8c3b0e79c572e037f203897b5ab4e61689936804
Macharia <gituikumacharia2@gmail.com>
2019-02-26 09:52:33 +0300

    adding webBuilder implementation

--- src/main/java/com/example/resttemplate/parser/BinFetcherController.java
+++ src/main/java/com/example/resttemplate/parser/BinFetcherController.java
@@ -9,6 +9,7 @@ import com.example.resttemplate.utils.Constants;
 import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  *
@@ -28,9 +30,15 @@ import org.springframework.web.client.RestTemplate;
 public class BinFetcherController {
 
     @Value("${spring.application.name}")
-    String appName;
+    private String appName;
 
-    String result;
+    private String result;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -69,18 +77,37 @@ public class BinFetcherController {
         }
         return result;
     }
-    
+
     //option without headers
     @GetMapping("/bin/cardnumber/{cardnumber}")
     public String validateCardData(@PathVariable String cardnumber) {
 
-        RestTemplate restTemplate;
         try {
             restTemplate = new RestTemplate();
 
             logger.debug(appName + " Sending 'POST' request to URL : " + Constants.URL);
 
             result = restTemplate.getForObject(Constants.URL + cardnumber, String.class);
+
+            logger.debug(appName + " Response from Bin Validator : " + result);
+        } catch (Exception ex) {
+            logger.error(appName + " Error Message : " + ex.getMessage());
+        }
+        return result;
+    }
+
+    //using web client builder
+    @GetMapping("/bin/cardnumber/webclient/{cardnumber}")
+    public String validateCard(@PathVariable String cardnumber) {
+        try {
+            result = webClientBuilder.build()
+                    .get()
+                    .uri(Constants.URL + cardnumber, String.class)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            logger.debug(appName + " Sending 'POST' request to URL : " + Constants.URL);
 
             logger.debug(appName + " Response from Bin Validator : " + result);
         } catch (Exception ex) {
